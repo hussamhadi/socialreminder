72ba01a3fc32d5dcf595569bfabe6e36760f4284
Hussam Hadi <hussam.hadi@teamrockstars.nl>
2022-02-06 22:50:14 +0100

    added frequency to Person model, tags to template

--- socialreminder/models.py
+++ socialreminder/models.py
@@ -15,13 +15,29 @@ class Person(models.Model):
     """
     Represents a person
     """
+    FREQUENCY = (
+        ('daily', 'Daily'),
+        ('weekly mo', 'Weekly on Monday'),
+        ('weekly tu', 'Weekly on Tuesday'),
+        ('weekly we', 'Weekly on Wednesday'),
+        ('weekly thu', 'Weekly on Thursday'),
+        ('weekly fr', 'Weekly on Friday'),
+        ('weekly sa', 'Weekly on Saturday'),
+        ('weekly su', 'Weekly on Sunday'),
+        ('monthly', 'Monthly'),
+    )
+
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     birthday = models.DateField()
+    frequency = models.CharField(choices=FREQUENCY, blank=True, null=True, max_length=30)
     tag = models.ManyToManyField(Tag, related_name="person", blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    def get_frequency(self):
+        return dict(self.FREQUENCY)[self.frequency]
 
     def get_tags(self):
         return "to be defined"
